82c36fd847a46a5840990f97746f17d407508d94
vknanavati <vimin.nanavati@gmail.com>
2022-12-29 11:36:53 -0500

    added data storage file

---
+++ data_storage.py
@@ -0,0 +1,300 @@
+state_list = [
+    {
+        "Name": "Alabama",
+        "Capital": "Montgomery",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Alaska",
+        "Capital": "Juneau",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Arizona",
+        "Capital": "Phoenix",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Arkansas",
+        "Capital": "Litte Rock",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "California",
+        "Capital": "Sacramento",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Colorado",
+        "Capital": "Denver",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Connecticut",
+        "Capital": "Hartford",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Delaware",
+        "Capital": "Dover",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Florida",
+        "Capital": "Tallahassee",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Georgia",
+        "Capital": "Atlanta",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Hawaii",
+        "Capital": "Honolulu",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Idaho",
+        "Capital": "Boise",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Illinois",
+        "Capital": "Springfield",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Indiana",
+        "Capital": "Indianapolis",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Iowa",
+        "Capital": "Ds Moines",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Kansas",
+        "Capital": "Topeka",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Kentucky",
+        "Capital": "Frankfort",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Louisiana",
+        "Capital": "Batn Rouge",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Maine",
+        "Capital": "Augusta",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Maryland",
+        "Capital": "Annapolis",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Massachusetts",
+        "Capital": "Boston",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Michigan",
+        "Capital": "Lansing",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Minnesota",
+        "Capital": "Sait Paul",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Mississippi",
+        "Capital": "Jackson",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Missouri",
+        "Capital": "Jeffersn City",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Montana",
+        "Capital": "Helena",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Nebraska",
+        "Capital": "Lincoln",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Nevada",
+        "Capital": "Carsn City",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "New Hampshire",
+        "Capital": "Concord",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "New Jersey",
+        "Capital": "Trenton",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "New Mexico",
+        "Capital": "SanaFe",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "New York",
+        "Capital": "Albany",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "North Carolina",
+        "Capital": "Raleigh",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "North Dakota",
+        "Capital": "Bismarck",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Ohio",
+        "Capital": "Columbus",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Oklahoma",
+        "Capital": "Oklahoa City",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Oregon",
+        "Capital": "Salem",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Pennsylvania",
+        "Capital": "Harrisburg",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Rhode Island",
+        "Capital": "Providence",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "South Carolina",
+        "Capital": "Columbia",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "South Dakota",
+        "Capital": "Pierre",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Tennessee",
+        "Capital": "Nashville",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Texas",
+        "Capital": "Austin",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Utah",
+        "Capital": "Salt Lae City",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Vermont",
+        "Capital": "Montpelier",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Washington",
+        "Capital": "Olympia",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "West Virginia",
+        "Capital": "Charleston",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {
+        "Name": "Wisconsin",
+        "Capital": "Madison",
+        "Population": 0000,
+        "Best Universities": ["College1", "College2", "College3"],
+    },
+    {"Name": "Wyoming", "Capital": "Cheyenne", "Population": 800000},
+]
+
+
+def printStates():
+    for state in state_list:
+        # print(state["Capital"])
+        print(state["Name"])
+
+
+printStates()
