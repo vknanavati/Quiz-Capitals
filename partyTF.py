571b9b3af5386e8cb65842df272e837a0f2a4934
vknanavati <vimin.nanavati@gmail.com>
2023-01-05 14:37:35 -0500

    trying to parse through each dictionary in the list in order to access the key that equals True in each state.

--- partyTF.py
+++ partyTF.py
@@ -25,25 +25,39 @@ state_list = [
 ]
 
 
-def politicalParty():
+x = [d["Party"] for d in state_list]
+# retrieve value of "Party"
+# print(x)
+# output: [{'rep': True, 'dem': False}, {'rep': False, 'dem': True}, {'rep': False, 'dem': True}]
 
-    states = list(state_list.keys("Name"))
+x = map(lambda d: d["Party"], state_list)
+# another way to retrieve value of "Party"
+# print(list(x))
+# output: [{'rep': True, 'dem': False}, {'rep': False, 'dem': True}, {'rep': False, 'dem': True}]
 
-    while len(state_list) > 0:
-        state = random.choice(states)
-        party = 
-
-    # while len(state_list) > 0:
-    #     state_party_dict = random.choice(state_list)
-    #     state_party = state_party_dict["Name"]
-    #     state_color = state_party_dict["Party"]
-
-    #     user_guess = input("Is" + state_party + "Republican or Democract?")
-
-    #     if user_guess == :
-    #         print("Yay")
-    #     else:
-    #         print("boo")
+y = map(lambda d: d["Name"], state_list)
+print(list(y))
 
 
+################################
+# SCRATCH PAD:
 
+# def politicalParty():
+
+#     states = list(state_list.keys("Name"))
+
+#     while len(state_list) > 0:
+#         state = random.choice(states)
+#         party =
+
+#     while len(state_list) > 0:
+#         state_party_dict = random.choice(state_list)
+#         state_party = state_party_dict["Name"]
+#         state_color = state_party_dict["Party"]
+
+#         user_guess = input("Is" + state_party + "Republican or Democract?")
+
+#         if user_guess == :
+#             print("Yay")
+#         else:
+#             print("boo")
