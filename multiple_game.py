9469844c670123d323b7de1c4ce67e7341fab485
vknanavati <vimin.nanavati@gmail.com>
2023-01-26 19:02:57 -0700

    used enumerate to label MCQs, zipped lists together to paid letter choices with answers.

--- multiple_game.py
+++ multiple_game.py
@@ -19,131 +19,133 @@ state_dict_list = json.loads(state_dict_list)
 
 #     return
 
+
 print("Running first funciton")
 
 
-def number_of_rounds():
-    pass
-    # rounds = 5
-    # for _ in range(rounds)
+def main():
+    def right_answer():
+        # grab correct answer
+        while len(state_dict_list) > 0:
+            single_state_dict = random.choice(state_dict_list)
+            # Checking that it returns single_state_dict
+            # print(single_state_dict)
+            # return single_state_dict
+            correct_answer = {
+                "Capital": single_state_dict["Capital"],
+                "State": single_state_dict["Name"],
+            }
+            print(f"Correct answer is: {correct_answer['Capital']}")
+            return correct_answer
 
+    good_answer = right_answer()
+    print(f"The good answer is: {good_answer}")
 
-# run this for whole function
-# number_of_rounds()
+    def wrong_answers():
+        false_answers = []
+        capitals = []
+        for dic in state_dict_list:
+            capitals.append(dic["Capital"])
 
+        # now we need to create a list from the capitals list of 3 random capitals
+        false_answers.append(capitals[random.randrange(0, 50)])
+        false_answers.append(capitals[random.randrange(0, 50)])
+        false_answers.append(capitals[random.randrange(0, 50)])
+        # print(f"Wrong Answers: {false_answers}")
 
-def right_answer():
-    # grab correct answer
-    while len(state_dict_list) > 0:
-        single_state_dict = random.choice(state_dict_list)
-        # Checking that it returns single_state_dict
-        # print(single_state_dict)
-        # return single_state_dict
-        correct_answer = {
-            "Capital": single_state_dict["Capital"],
-            "State": single_state_dict["Name"],
-        }
-        print(f"Correct answer is: {correct_answer['Capital']}")
-        return correct_answer
+        false_answers = list(dict.fromkeys(false_answers))
 
+        if len(false_answers) < 3:
+            wrong_answers()
+        print(f"FALSE ANSWERS ARE: {false_answers}")
+        return false_answers
 
-good_answer = right_answer()
-print(f"The good answer is: {good_answer}")
+        # print(f"Wrong Answers: {false_answers}")
+        # return false_answers
 
+        # false_answers.append(capitals[random.randrange(0, 50)])
+        # false_answers.append(capitals[random.randrange(0, 50)])
+        # false_answers.append(capitals[random.randrange(0, 50)])
+        # print(f"Wrong Answers: {false_answers}")
+        # return false_answers
 
-def wrong_answers():
-    false_answers = []
-    capitals = []
-    for dic in state_dict_list:
-        capitals.append(dic["Capital"])
+    wrong_list = wrong_answers()
 
-    # now we need to create a list from the capitals list of 3 random capitals
-    false_answers.append(capitals[random.randrange(0, 50)])
-    false_answers.append(capitals[random.randrange(0, 50)])
-    false_answers.append(capitals[random.randrange(0, 50)])
-    # print(f"Wrong Answers: {false_answers}")
+    print(f"wrong list:{wrong_list}")
 
-    false_answers = list(dict.fromkeys(false_answers))
+    def validate_answers(right: dict[str, str], wrongs: list):
+        print(f"validate_answer arguments: {right}, {wrongs}")
+        print(f'right answer for capital: {right["Capital"]}')
+        for wrong_choice in wrongs:
+            if wrong_choice == right["Capital"]:
+                wrongs = wrong_answers()
 
-    if len(false_answers) < 3:
-        wrong_answers()
-    print(f"FALSE ANSWERS ARE: {false_answers}")
-    return false_answers
+        all_choices = {"Wrong Answers": wrongs, "Right Answer": right}  # []  # {}
 
-    # print(f"Wrong Answers: {false_answers}")
-    # return false_answers
+        print(f"Wrongs are validated: {all_choices['Wrong Answers']}")
+        return all_choices
 
-    # false_answers.append(capitals[random.randrange(0, 50)])
-    # false_answers.append(capitals[random.randrange(0, 50)])
-    # false_answers.append(capitals[random.randrange(0, 50)])
-    # print(f"Wrong Answers: {false_answers}")
-    # return false_answers
+    four_answers = validate_answers(good_answer, wrong_list)
 
+    print(f"four answers are: {four_answers}")
+    # output -> four answers are: {'Wrong Answers': ['Pierre', 'Harrisburg', 'Springfield'], 'Right Answer': {'Capital': 'Jackson', 'State': 'Mississippi'}}
 
-wrong_list = wrong_answers()
+    def playMultiple():
+        shuffle_list = []
+        three_caps = four_answers["Wrong Answers"]
+        one_cap = four_answers["Right Answer"]["Capital"]
+        new_line = "\n"
 
-print(f"wrong list:{wrong_list}")
+        print(f"Four choices: {one_cap}, {three_caps}")
 
+        for city in three_caps:
+            shuffle_list.append(city)
+        print(f"{new_line}SHUFFLED LIST IS: {shuffle_list}")
 
-def validate_answers(right: dict[str, str], wrongs: list):
-    print(f"validate_answer arguments: {right}, {wrongs}")
-    print(f'right answer for capital: {right["Capital"]}')
-    for wrong_choice in wrongs:
-        if wrong_choice == right["Capital"]:
-            wrongs = wrong_answers()
+        shuffle_list.append(four_answers["Right Answer"]["Capital"])
+        print(f"COMPLETED LIST FOR SUFFLE{shuffle_list}")
 
-    all_choices = {"Wrong Answers": wrongs, "Right Answer": right}  # []  # {}
+        shuffle_copied_list = shuffle_list.copy()
+        print(f"RANDOM LIST IS: {shuffle_copied_list}")
 
-    print(f"Wrongs are validated: {all_choices['Wrong Answers']}")
-    return all_choices
+        random.shuffle(shuffle_copied_list)
+        print(f"SHUFFLED RANDO LIST: {shuffle_copied_list}")
 
+        letters = ["a", "b", "c", "d"]
 
-four_answers = validate_answers(good_answer, wrong_list)
+        choices = zip(letters, shuffle_copied_list)
+        print(tuple(choices))
 
-print(f"four answers are: {four_answers}")
-# output -> four answers are: {'Wrong Answers': ['Pierre', 'Harrisburg', 'Springfield'], 'Right Answer': {'Capital': 'Jackson', 'State': 'Mississippi'}}
+        # wrong_list.append(good_answer)
+        # print(f"{new_line}COMBO_LIST is {wrong_list}")
 
-
-def playMultiple():
-    shuffle_list = []
-    three_caps = four_answers["Wrong Answers"]
-    one_cap = four_answers["Right Answer"]["Capital"]
-    new_line = "\n"
-
-    print(f"Four choices: {one_cap}, {three_caps}")
-
-    for city in three_caps:
-        shuffle_list.append(city)
-    print(f"{new_line}SHUFFLED LIST IS: {shuffle_list}")
-
-    shuffle_list.append(four_answers["Right Answer"]["Capital"])
-    print(f"COMPLETED LIST FOR SUFFLE{shuffle_list}")
-
-    shuffle_copied_list = shuffle_list.copy()
-    print(f"RANDOM LIST IS: {shuffle_copied_list}")
-
-    random.shuffle(shuffle_copied_list)
-    print(f"SHUFFLED RANDO LIST: {shuffle_copied_list}")
-
-    # wrong_list.append(good_answer)
-    # print(f"{new_line}COMBO_LIST is {wrong_list}")
-
-    user_guess = input(
-        f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(shuffle_copied_list)}{new_line}"
-    )
-
-    # user_guess = input(
-    #     f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(three_caps)}{new_line}{four_answers['Right Answer']['Capital']}{new_line}"
-    # )
-    if user_guess in one_cap:
-        print("Woohoo!")
-    else:
-        print(
-            f"I'm sorry the correct answer is {four_answers['Right Answer']['Capital']}"
+        user_guess = input(
+            f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(shuffle_copied_list)}{new_line}"
         )
 
+        # user_guess = input(
+        #     f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(three_caps)}{new_line}{four_answers['Right Answer']['Capital']}{new_line}"
+        # )
+        while len(user_guess) > 0:
+            if user_guess in one_cap:
+                print("Woohoo!")
+                return
+            print(
+                f"I'm sorry the correct answer is {four_answers['Right Answer']['Capital']}"
+            )
+            return
 
-playMultiple()
+    playMultiple()
+
+
+def number_of_rounds():
+    for _ in range(5):
+        main()
+
+
+# number_of_rounds()
+# run this for whole function
+number_of_rounds()
 
 
 # NEXT:
