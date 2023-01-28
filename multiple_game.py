ed4782c11496c49742a8233fccd3dd319336592d
vknanavati <vimin.nanavati@gmail.com>
2023-01-28 15:44:12 -0700

    worked on playMultiple function.

--- multiple_game.py
+++ multiple_game.py
@@ -8,21 +8,6 @@ with open("dummy_multiple_data.json", encoding="UTF-8") as state_dict_list:
 state_dict_list = json.loads(state_dict_list)
 
 
-# def multiple_choice_game():
-#     # state = single_state_dict["Name"]
-#     # print(state)
-#     # return state
-#     # quest = f"What is the capital of {state} ? "
-#     # print(quest)
-#     # return quest
-#     # need to put ONLY capitals into a list
-
-#     return
-
-
-print("Running first funciton")
-
-
 def main():
     def right_answer():
         # grab correct answer
@@ -90,56 +75,81 @@ def main():
     print(f"four answers are: {four_answers}")
     # output -> four answers are: {'Wrong Answers': ['Pierre', 'Harrisburg', 'Springfield'], 'Right Answer': {'Capital': 'Jackson', 'State': 'Mississippi'}}
 
-    def playMultiple():
-        shuffle_list = []
-        three_caps = four_answers["Wrong Answers"]
-        one_cap = four_answers["Right Answer"]["Capital"]
+    def playMultiple(answers):
         new_line = "\n"
+        choices = answers["Wrong Answers"]
+        choices.append(answers["Right Answer"]["Capital"])
 
-        print(f"Four choices: {one_cap}, {three_caps}")
+        random.shuffle(choices)
 
-        for city in three_caps:
-            shuffle_list.append(city)
-        print(f"{new_line}SHUFFLED LIST IS: {shuffle_list}")
+        selections = f"'\n' a. {choices[0]} '\n' b. {choices[1]} '\n' c. {choices[2]} '\n' d. {choices[3]}"
 
-        shuffle_list.append(four_answers["Right Answer"]["Capital"])
-        print(f"COMPLETED LIST FOR SUFFLE{shuffle_list}")
+        choice_map = {
+            "a": choices[0],
+            "b": choices[1],
+            "c": choices[2],
+            "d": choices[3],
+        }
+        # choice_map[user_guess] => choice_map['a'] => 'Trenton'
 
-        shuffle_copied_list = shuffle_list.copy()
-        print(f"RANDOM LIST IS: {shuffle_copied_list}")
-
-        random.shuffle(shuffle_copied_list)
-        print(f"SHUFFLED RANDO LIST: {shuffle_copied_list}")
-
-        letters = ["a", "b", "c", "d"]
-
-        # choices = zip(letters, shuffle_copied_list)
-        # print(list(tuple(choices)))
-
-        wrong_list.append(good_answer)
-        # print(f"{new_line}COMBO_LIST is {wrong_list}")
-
-        print("Letters paired with options:")
-        for l, s in zip(letters, shuffle_copied_list):
-            print(l, s)
+        # print(f"selections: {selections}")
 
         user_guess = input(
-            f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(shuffle_copied_list)}{new_line}"
+            f"{new_line}What is the capital of {answers['Right Answer']['State']} ? {selections} '\n' '\n'"
         )
 
-        # user_guess = input(
-        #     f"{new_line}What is the capital of {four_answers['Right Answer']['State']} ?{new_line} {new_line}{new_line.join(three_caps)}{new_line}{four_answers['Right Answer']['Capital']}{new_line}"
-        # )
-        while len(user_guess) > 0:
-            if user_guess in one_cap:
-                print("Woohoo!")
-                return
-            print(
-                f"I'm sorry the correct answer is {four_answers['Right Answer']['Capital']}"
-            )
-            return
+        if (
+            user_guess != "a"
+            or user_guess != "b"
+            or user_guess != "c"
+            or user_guess != "d"
+        ):
+            print("Sorry that choice is not available")
 
-    playMultiple()
+        if choice_map[user_guess] == answers["Right Answer"]["Capital"]:
+            print(f"Correct! {answers['Right Answer']['Capital']}")
+        else:
+            print(f"Nope! The correct answer is {answers['Right Answer']['Capital']}")
+
+        return
+
+    playMultiple(four_answers)
+
+    # def playMultiple():
+    #     three_caps = four_answers["Wrong Answers"]
+    #     one_cap = four_answers["Right Answer"]["Capital"]
+    #     state = four_answers["Right Answer"]["State"]
+    #     new_line = "\n"
+
+    #     print(f"{new_line}One cap: {one_cap}{new_line}")
+
+    #     print(f"Three cap:{three_caps}{new_line}")
+
+    #     three_caps.append(one_cap)
+    #     print(three_caps)
+
+    #     random.shuffle(three_caps)
+    #     print(f"{new_line}Three caps shuffled: {three_caps}{new_line}")
+
+    #     # letters = ["a", "b", "c", "d"]
+
+    #     # print("Letters paired with options:")
+    #     # for l, s in zip(letters, three_caps):
+    #     #     print(l, s)
+
+    #     # user_guess = input(f"{new_line}What is the capital of {state} ?")
+
+    #     # {new_line} {new_line}{new_line.join(shuffle_copied_list)}{new_line}
+
+    #     user_guess = input(
+    #         f"{new_line}What is the capital of {state} ?{new_line} {new_line}{new_line.join(three_caps)}{new_line}"
+    #     )
+    #     while len(user_guess) > 0:
+    #         if user_guess in one_cap:
+    #             print("Woohoo!")
+    #             return
+    #         print(f"I'm sorry the correct answer is {one_cap}")
+    #         return
 
 
 def number_of_rounds():
@@ -151,79 +161,11 @@ def number_of_rounds():
 # run this for whole function
 number_of_rounds()
 
-
 # NEXT:
-# Check final answers list for duplicate choices
-# def playMultiple():
-#   add multiple choice questions in A, B, C, D format
-#   compare user input to correct answer
-# make sure edge case in duplicate answer in final_answers is resolved
-# run for 5 questions
-#####################################################################################
-# DONE TODAY:
-# 1. DRILLED stateApp game
-# 2. DRILLED MCQ app
-# 3. to def playMultiple - I added correct answer to appear among possible choices
-## 4. created question and user input
-## 5. added if statements to confirm if answer is correct and also to reveal correct answer if user_guess is incorrect
-
-##########################################
-# def multiple_choice_game():
-#     capitals = []
-#     wrong_answers = []
-
-#     def multiple_answers():
-#         for dic in state_dict_list:
-#             capitals.append(dic["Capital"])
-
-#         wrong_answers.append(capitals[random.randrange(0, 50)])
-#         wrong_answers.append(capitals[random.randrange(0, 50)])
-#         wrong_answers.append(capitals[random.randrange(0, 50)])
-
-#         print(f"Wrong Answers: {wrong_answers}")
-#         return wrong_answers
-
-#     wrong_answers = multiple_answers()
-#     # Validation: check all 3 wrong answers do not match the correct answer
-#     for choice in wrong_answers:
-#         if choice == "Trenton":
-#             wrong_answers = []
-#             wrong_answers = multiple_answers()
-
-#     print("Wrong answers validated")
-
-#     return wrong_answers
-
-
-# multiple_choice_game()
-
-#################################################
-# def Wrong_Answer_List():
-
-#     wrong_cap_list = state_dict_list[Capital]
-#     print(wrong_cap_list)
-
-
-# Wrong_Answer_List()
-
-#     w_answer = []
-#     count = 0
-
-
-# for x in state_list["Capital"]
-# #####################################################
-
-# def capitalGame():
-#     while len(state_dict_list) > 0:
-#         single_state_dict = random.choice(state_dict_list)
-#         state = single_state_dict["Name"]
-#         capital = single_state_dict["Capital"]
-
-#         user_guess = input("What is the capital of " + state + "? ")
-
-#         if user_guess == capital:
-#             print("Woohoo! You got it.")
-#         elif user_guess not in capital:
-#             print("Oh no, your answer is incorrect.")
-#         else:
-#             print("Please write republican or democratic.")
+# fruits and colors map
+# re-write script that seb wrote
+# re-write entire script
+# handle validation - clicking wrong letter
+# remove ' characters from answer choices
+# handle duplicate wrong answer in choices
+# incorporate into larger app
