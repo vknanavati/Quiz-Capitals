14a1b58f37379ba3044b543e119df1ed47baf6ad
vknanavati <vimin.nanavati@gmail.com>
2023-01-29 15:50:18 -0700

    edited play_multiple().

--- multiple_game.py
+++ multiple_game.py
@@ -20,7 +20,7 @@ def main():
                 "Capital": single_state_dict["Capital"],
                 "State": single_state_dict["Name"],
             }
-            print(f"\nCorrect answer is: {correct_answer['Capital']}\n")
+            # print(f"\nCorrect answer is: {correct_answer['Capital']}\n")
             return correct_answer
 
     good_answer = right_answer()
@@ -33,15 +33,15 @@ def main():
             capitals.append(dic["Capital"])
 
         # now we need to create a list from the capitals list of 3 random capitals
-        false_answers.append(capitals[random.randrange(0, 50)])
-        false_answers.append(capitals[random.randrange(0, 50)])
-        false_answers.append(capitals[random.randrange(0, 50)])
+        false_answers.append(capitals[random.randrange(49)])
+        false_answers.append(capitals[random.randrange(49)])
+        false_answers.append(capitals[random.randrange(49)])
 
-        print(f"\nBefore fromkeys method: {false_answers}\n")
+        # print(f"\nBefore fromkeys method: {false_answers}\n")
 
         false_answers = list(dict.fromkeys(false_answers))
 
-        print(f"\nAfter fromkeys: {false_answers}\n")
+        # print(f"\nAfter fromkeys: {false_answers}\n")
 
         if len(false_answers) < 3:
             wrong_answers()
@@ -50,20 +50,20 @@ def main():
     wrong_list = wrong_answers()
 
     def validate_answers(right: dict[str, str], wrongs: list):
-        print(f"validate_answer arguments: {right}, {wrongs}")
-        print(f'right answer for capital: {right["Capital"]}')
+        # print(f"validate_answer arguments: {right}, {wrongs}")
+        # print(f'right answer for capital: {right["Capital"]}')
         for wrong_choice in wrongs:
             if wrong_choice == right["Capital"]:
                 wrongs = wrong_answers()
 
         all_choices = {"Wrong Answers": wrongs, "Right Answer": right}  # []  # {}
 
-        print(f"Wrongs are validated: {all_choices['Wrong Answers']}")
+        # print(f"Wrongs are validated: {all_choices['Wrong Answers']}")
         return all_choices
 
     four_answers = validate_answers(good_answer, wrong_list)
 
-    print(f"four answers are: {four_answers}")
+    # print(f"four answers are: {four_answers}")
     # output -> four answers are: {'Wrong Answers': ['Pierre', 'Harrisburg', 'Springfield'], 'Right Answer': {'Capital': 'Jackson', 'State': 'Mississippi'}}
 
     def play_multiple(answers):
@@ -80,28 +80,29 @@ def main():
             "c": choices[2],
             "d": choices[3],
         }
+
         # choice_map[user_guess] => choice_map['a'] => 'Trenton'
 
         # print(f"selections: {selections}")
 
-        user_guess = input(
+        # user_guess = input(
+        #     f"\nWhat is the capital of {answers['Right Answher']['State']} ? \n{selections} \n \n"
+        # )
+        print(
             f"\nWhat is the capital of {answers['Right Answer']['State']} ? \n{selections} \n \n"
         )
 
-        if (
-            user_guess != "a"
-            or user_guess != "b"
-            or user_guess != "c"
-            or user_guess != "d"
-        ):
-            print("Sorry that choice is not available")
+        user_guess = input()
+
+        print(choice_map[user_guess])
 
         if choice_map[user_guess] == answers["Right Answer"]["Capital"]:
             print(f"Correct! {answers['Right Answer']['Capital']}")
-        else:
+        elif choice_map[user_guess] not in answers["Right Answer"]["Capital"]:
             print(f"Nope! The correct answer is {answers['Right Answer']['Capital']}")
-
-        return
+        else:
+            print("Incorrect character, try again")
+            print(user_guess)
 
     play_multiple(four_answers)
 
@@ -111,18 +112,19 @@ def number_of_rounds():
         main()
 
 
-# number_of_rounds()
-# run this for whole function
 number_of_rounds()
 
 # Did today Jan 29 Sunday:
 # # removed single quotes form \n entries in fstring of playMultiple()
 # # in wrong_answers() created if statement to rerun function if duplicates in false_answers
+# # fixed range error in wrong_answers rrange argument should be 49
 
 # NEXT:
 # fruits and colors map
 # re-write script that seb wrote
 # re-write entire script
-# handle validation - clicking wrong letter
+# handle validation - if user clicks wrong letter
+# handle allowing for user to guess again if incorrect character entered
+# handle moving onto next question if wrong answer entered
 # handle .capitilize for answer choices
 # incorporate into larger app
